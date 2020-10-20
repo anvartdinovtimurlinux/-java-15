81238b3615e63b28f2d281c10c8e386bc184a199
Anvartdinov Timur <AnvartdinovTimurLinux@gmail.com>
2020-10-20 10:15:41 +0300

    вторая задача

---
+++ generics/generics_2/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // параметризируем класс типом String для ключа и значения
+        Box<String, String> sample1 = new Box<>("имя", "Нетология");
+        System.out.println(sample1);
+
+        // параметризируем класс типом Integer для ключа и Boolean для значения
+        Box<Integer, Boolean> sample2 = new Box<>(1, Boolean.TRUE);
+        System.out.println(sample2);
+
+
+        ArrayList<Box<String, Integer>> list1 = new ArrayList<>();
+        list1.add(sample1);
+        list1.add(sample2);
+        // параметры Box, хранящегося в list1 не соответсвуют параметрам, добавляемых туда значений
+        // <String, String> != <String, Integer> && <Integer, Boolean> != <String, Integer>
+    }
+
+}
